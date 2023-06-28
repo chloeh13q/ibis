3917179f7abbdacd4fb67748dd1be82fe2750105
Chloe He <chloehe47@gmail.com>
2023-06-28 15:19:32 -0700

    feat(flink): add tests and translation rules for additional operators

--- ibis/backends/flink/tests/test_translator.py
+++ ibis/backends/flink/tests/test_translator.py
@@ -50,3 +50,81 @@ def test_translate_timestamp_from_unix(snapshot, table, unit):
     expr = table.d.to_timestamp(unit=unit)
     result = translate(expr.as_table().op())
     snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_complex_projections(snapshot, table):
+    expr = (
+        table.group_by(['a', 'c'])
+        .aggregate(the_sum=table.b.sum())
+        .group_by('a')
+        .aggregate(mad=lambda x: x.the_sum.abs().mean())
+    )
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_filter(snapshot, table):
+    expr = table[((table.c > 0) | (table.c < 0)) & table.g.isin(['A', 'B'])]
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+@pytest.mark.parametrize(
+    "kind",
+    [
+        "year",
+        "quarter",
+        "month",
+        "week_of_year",
+        "day_of_year",
+        "day",
+        "hour",
+        "minute",
+        "second",
+    ],
+)
+def test_translate_extract_fields(snapshot, table, kind):
+    expr = getattr(table.i, kind)().name("tmp")
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_complex_groupby_aggregation(snapshot, table):
+    keys = [table.i.year().name('year'), table.i.month().name('month')]
+    b_unique = table.b.nunique()
+    expr = table.group_by(keys).aggregate(total=table.count(), b_unique=b_unique)
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_simple_filtered_agg(snapshot, table):
+    expr = table.b.nunique(where=table.g == 'A')
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_complex_filtered_agg(snapshot, table):
+    expr = table.group_by('b').aggregate(
+        total=table.count(),
+        avg_a=table.a.mean(),
+        avg_a_A=table.a.mean(where=table.g == 'A'),
+        avg_a_B=table.a.mean(where=table.g == 'B'),
+    )
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_value_counts(snapshot, table):
+    expr = table.i.year().value_counts()
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
+
+
+def test_translate_having(snapshot, table):
+    expr = (
+        table.group_by('g')
+        .having(table.count() >= 1000)
+        .aggregate(table.b.sum().name('b_sum'))
+    )
+    result = translate(expr.as_table().op())
+    snapshot.assert_match(result, "out.sql")
